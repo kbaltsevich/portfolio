3ad0256448950b387d4e0f8fec0daf4e4d1b6d86
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2020-12-30 13:52:11 +0300

    feat: create scss files, add smooth scroll

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const menuVisibleButon = document.querySelector('.header__menu-img')
 const nav = document.querySelector('.nav')
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-
-menuVisibleButon.addEventListener('click', () => {
+const visibleBurgerMenu = () => {
     menuVisibleButon.classList.toggle('header__menu-img--rotate')
     return nav.classList.toggle('nav__visible')
-})
+}
+
+
+menuVisibleButon.addEventListener('click', visibleBurgerMenu)
+
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+
+    visibleBurgerMenu()
+  })
+}
+
